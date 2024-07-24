bb791d0c4af685b3b57987d372bfcec76e501452
Avertae <alex.gorbanev@gmail.com>
2024-07-24 02:25:29 +0200

    Improved DSU template

--- leetcode/templates/DSU.java
+++ leetcode/templates/DSU.java
@@ -2,33 +2,64 @@
         Map<Integer, Integer> parent = new HashMap<>();
         Map<Integer, Integer> size = new HashMap<>();
 
-        public void makeSet(int i) {
-            parent.put(i, i);
-            size.put(i, 1);
+        public void add(int x) {
+            parent.put(x, x);
+            size.put(x, 1);
         }
 
-        public Integer findSet(int i) {
-            if (!parent.keySet().contains(i)) {
-                return null;
-            }
-            if (parent.get(i) == i) {
-                return i;
-            }
-
-            parent.put(i, findSet(parent.get(i)));
-            return parent.get(i);
+        public int find(int x) {
+            int px = parent.get(x);
+            if (x != px) 
+                parent.put(x, find(px));
+            return px;
         }
 
-        public void unionSet(int i, int j) {
-            i = findSet(i);
-            j = findSet(j);
-            if (size.get(i) < size.get(j)) {
-                int tmp = i;
-                i = j;
-                j = tmp;
-            }
+        public boolean contains(int x) {
+            return parent.containsKey(x);
+        }
 
-            parent.put(j, i);
-            size.put(i, size.get(i) + size.get(j));
+        public void union(Integer x, Integer y) {
+            if (!contains(x) || !contains(y))
+                return;
+
+            x = find(x);
+            y = find(y);
+
+            if (x == y)
+                return;
+
+            int sx = size.get(x);
+            int sy = size.get(y);
+            if (sx < sy) {
+                int tmp = x;
+                x = y;
+                y = tmp;
+            }
+            parent.put(y, x);
+            size.put(x, sx + sy);
+        }
+
+        public int size(int x) {
+            return size.get(x);
+        }
+
+        public int maxUnion() {
+            return parent.keySet().stream()
+                .mapToInt(x -> size(find(x)))
+                .max()
+                .orElse(0);
         }
     }
+
+    /*  
+        for (int x : set) {
+            if (!dsu.contains(x))
+                dsu.add(x);
+            if (dsu.contains(x - 1))
+                dsu.union(x, x - 1);
+            if (dsu.contains(x + 1))
+                dsu.union(x, x + 1);
+        }
+
+        return dsu.maxUnion(); 
+    */
